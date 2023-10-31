dbcbdfdd7bb535f91635f8beb6ab1981b7f9ce4e
Jon Greazel <jdgreazel@gmail.com>
2023-10-31 10:36:42 -0500

    db mysql

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,9 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
