e07c70b357d5df3465f7b3d73a9187e7067c5e25
Jon Greazel <jdgreazel@gmail.com>
2024-02-20 18:22:01 -0600

    add admission events to report router get new

--- src/server/api/routers/reports.ts
+++ src/server/api/routers/reports.ts
@@ -15,30 +15,41 @@ const ratelimit = new Ratelimit({
 });
 
 export const reportsRouter = createTRPCRouter({
+  // todo: only query db if input params dictate so
   getNew: privateProcedure
     .input(
       z.object({
-        purchaseReport: z.object({
-          startDate: z.date(),
-          endDate: z.date(),
-          includeAdmissions: z.boolean().default(false).optional(),
-          includeConcessions: z.boolean().default(false).optional(),
-        }),
-        // todo
-        admissionReport: z.object({}).optional().nullable(),
+        purchaseReport: z
+          .object({
+            startDate: z.date(),
+            endDate: z.date(),
+            includeAdmissions: z.boolean().default(false).optional(),
+            includeConcessions: z.boolean().default(false).optional(),
+          })
+          .optional()
+          .nullable(),
+        admissionReport: z
+          .object({
+            startDate: z.date(),
+            endDate: z.date(),
+          })
+          .optional()
+          .nullable(),
       }),
     )
     .query(async ({ ctx, input }) => {
+      // Rate Limit check
       const createdById = ctx.userId;
       const { success } = await ratelimit.limit(createdById);
       if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
+      // Purchase Report
       const tranItemLinks = await ctx.db.transactionItems
         .findMany({
           where: {
             createdAt: {
-              gte: input.purchaseReport.startDate,
-              lte: input.purchaseReport.endDate,
+              gte: input.purchaseReport?.startDate,
+              lte: input.purchaseReport?.endDate,
             },
           },
           include: {
@@ -48,9 +59,9 @@ export const reportsRouter = createTRPCRouter({
         .then((dbLinks) =>
           dbLinks.filter((l) => {
             if (l.item.isAdmissionItem) {
-              return input.purchaseReport.includeAdmissions;
+              return input.purchaseReport?.includeAdmissions;
             } else if (l.item.isConcessionItem) {
-              return input.purchaseReport.includeConcessions;
+              return input.purchaseReport?.includeConcessions;
             }
           }),
         );
@@ -76,10 +87,20 @@ export const reportsRouter = createTRPCRouter({
           users.find((u) => u.id === l.createdBy)?.username ?? l.createdBy;
       });
 
+      // Admission Report
+      const adEvents = ctx.db.admissionEvent.findMany({
+        where: {
+          createdAt: {
+            gte: input.admissionReport?.startDate,
+            lte: input.admissionReport?.endDate,
+          },
+        },
+      });
+
       return {
-        purchaseReport: {
-          startDate: input.purchaseReport.startDate,
-          endDate: input.purchaseReport.endDate,
+        purchaseReport: !!input.purchaseReport && {
+          startDate: input.purchaseReport?.startDate,
+          endDate: input.purchaseReport?.endDate,
           transactions: tranItemLinks,
           summary: {
             admissionTotal,
@@ -87,6 +108,11 @@ export const reportsRouter = createTRPCRouter({
             concessionTotal,
             concessionCount,
           },
+        },
+        admissionReport: !!input.admissionReport && {
+          startDate: input.admissionReport?.startDate,
+          endDate: input.admissionReport?.endDate,
+          admissionEvents: adEvents,
         },
       };
     }),
