a0664a59b9130c5b79850a206f1a7ccc8a12448e
Jon Greazel <jdgreazel@gmail.com>
2024-02-23 18:41:41 -0600

    radio, only had 1 huh?

--- src/pages/items/[id].tsx
+++ src/pages/items/[id].tsx
@@ -81,7 +81,7 @@ const AdmissionItemForm = (props: {
           type="radio"
           value="day"
           {...register("passType")}
-          className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+          className="radio"
         />
         <label
           htmlFor="passTypeDayPassOption"
@@ -96,7 +96,7 @@ const AdmissionItemForm = (props: {
           type="radio"
           value="seasonal"
           {...register("passType")}
-          className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+          className="radio"
         />
         <label
           htmlFor="passTypeSeasonalPassOption"
