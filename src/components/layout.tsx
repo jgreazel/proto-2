caf35f92c9d68f636b0262e8341ba793052d61ba
Jon Greazel <jdgreazel@gmail.com>
2024-03-27 17:08:37 -0500

    begin work for feedback component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,71 @@
 import { SignOutButton, useUser } from "@clerk/nextjs";
-import type { PropsWithChildren, ReactNode } from "react";
+import { useState, type PropsWithChildren, type ReactNode } from "react";
 import Link from "next/link";
 
 type LayoutProps = {
   actionRow?: ReactNode;
   hideHeader?: boolean;
+};
+
+const Feedback = ({ onClose }: { onClose: () => void }) => {
+  // todo: create db and api
+  const [val, setVal] = useState("");
+  return (
+    <dialog className="modal modal-open">
+      <form method="dialog" className="modal-backdrop">
+        <button onClick={onClose}>close</button>
+      </form>
+      <div className="modal-box flex flex-col gap-2">
+        <form method="dialog">
+          <button
+            onClick={onClose}
+            className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </form>
+        <div className="font-medium">Leave Feedback</div>
+        <textarea
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+          className="textarea textarea-bordered w-full"
+          placeholder="Experiencing bugs or have an idea?"
+        ></textarea>
+        <div className="flex justify-end">
+          <button className="btn btn-primary">
+            Submit
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
 };
 
 const LinkListItems = () => {
@@ -149,84 +210,108 @@ const LinkListItems = () => {
 
 const FullNav = () => {
   const { user, isLoaded: userLoaded, isSignedIn } = useUser();
+  const [show, setShow] = useState(false);
 
   // user should load fast, just return empty until then
   if (!userLoaded) return <div></div>;
 
   return (
-    <div className="navbar rounded-lg bg-base-100 shadow">
-      {/* // small screens */}
-      <div className="navbar-start">
-        <div className="dropdown z-50">
-          <div tabIndex={0} role="button" className="btn btn-ghost">
+    <>
+      <div className="navbar rounded-lg bg-base-100 shadow">
+        {/* // small screens */}
+        <div className="navbar-start">
+          <div className="dropdown z-50">
+            <div tabIndex={0} role="button" className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content menu-md z-[1] mt-3 w-52 rounded-box bg-base-200 p-2 shadow-xl"
+            >
+              <LinkListItems />
+            </ul>
+          </div>
+          <Link href="/" className="btn btn-ghost text-xl">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
+              strokeWidth={1.5}
               stroke="currentColor"
+              className="h-6 w-6"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu dropdown-content menu-md z-[1] mt-3 w-52 rounded-box bg-base-200 p-2 shadow-xl"
-          >
-            <LinkListItems />
-          </ul>
+            Guard Shack
+          </Link>
         </div>
-        <Link href="/" className="btn btn-ghost text-xl">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
-          Guard Shack
-        </Link>
-      </div>
-      {/* // full width */}
-      {/* <div className="navbar-center hidden lg:flex">
+        {/* // full width */}
+        {/* <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <LinkListItems />
         </ul>
       </div> */}
-      <div className="navbar-end">
-        {!!isSignedIn && (
-          <div className="dropdown dropdown-end z-50">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost capitalize"
-            >
-              Hi, {user.username}
+        <div className="navbar-end">
+          {!!isSignedIn && (
+            <div className="dropdown dropdown-end z-50">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost capitalize"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu dropdown-content menu-md rounded-box bg-base-100 p-2 shadow-xl"
+              >
+                <div className="p-1 font-medium capitalize">
+                  Hi, {user.username}
+                </div>
+                <div className="divider m-0"></div>
+                <li className="w-max">
+                  <SignOutButton />
+                </li>
+                <li className="w-max" onClick={() => setShow(true)}>
+                  <a>Give Feedback</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu dropdown-content menu-md rounded-box bg-base-200 p-2 shadow-xl"
-            >
-              <li className="w-max">
-                <SignOutButton />
-              </li>
-            </ul>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+      {show && <Feedback onClose={() => setShow(false)} />}
+    </>
   );
 };
 
--- src/pages/passes/patrons/[id].tsx
+++ src/pages/passes/patrons/[id].tsx
@@ -27,58 +27,60 @@ export default function SinglePatronPage() {
   );
 
   return (
-    <dialog id="single-item-modal" className="modal modal-open">
-      <form method="dialog" className="modal-backdrop">
-        <Link href="/passes">close</Link>
-      </form>
-      <div className="modal-box">
-        <form method="dialog">
-          <Link
-            href="/passes"
-            className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
-          </Link>
+    <PageLayout>
+      <dialog id="single-item-modal" className="modal modal-open">
+        <form method="dialog" className="modal-backdrop">
+          <Link href="/passes">close</Link>
         </form>
+        <div className="modal-box">
+          <form method="dialog">
+            <Link
+              href="/passes"
+              className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </Link>
+          </form>
 
-        <PatronForm
-          data={{
-            id: data?.id,
-            firstName: data?.firstName ?? "",
-            lastName: data?.lastName ?? "",
-            birthDate: dayjs(data?.birthDate),
-          }}
-          disabled={isLoading || isUpdating}
-          onCancel={() => {
-            router.back();
-          }}
-          submitText="Update"
-          onSubmit={(data) => {
-            if (!data.id) {
-              toast.error("Missing patron id. Cannot update database");
-            } else {
-              mutate({
-                ...data,
-                id: data.id,
-                birthDate: data.birthDate.toDate(),
-              });
-            }
-          }}
-        />
-      </div>
-    </dialog>
+          <PatronForm
+            data={{
+              id: data?.id,
+              firstName: data?.firstName ?? "",
+              lastName: data?.lastName ?? "",
+              birthDate: dayjs(data?.birthDate),
+            }}
+            disabled={isLoading || isUpdating}
+            onCancel={() => {
+              router.back();
+            }}
+            submitText="Update"
+            onSubmit={(data) => {
+              if (!data.id) {
+                toast.error("Missing patron id. Cannot update database");
+              } else {
+                mutate({
+                  ...data,
+                  id: data.id,
+                  birthDate: data.birthDate.toDate(),
+                });
+              }
+            }}
+          />
+        </div>
+      </dialog>
+    </PageLayout>
   );
 }
