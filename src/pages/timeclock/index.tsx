19954c54772cb3711dbed72fbddc4955ba877c50
Jon Greazel <jdgreazel@gmail.com>
2024-04-16 21:48:17 -0500

    bootstrap timecard page

---
+++ src/pages/timeclock/index.tsx
@@ -0,0 +1,19 @@
+import { PageLayout } from "~/components/layout";
+
+// todo add clockPIN to userSettings
+
+// pull all shifts for today
+// show each user with their shifts for today, button to punch time card
+// creates clock event
+
+const ShiftFeed = () => {
+  return <div>feed</div>;
+};
+
+export default function TimeClockPage() {
+  return (
+    <PageLayout>
+      <ShiftFeed />
+    </PageLayout>
+  );
+}
