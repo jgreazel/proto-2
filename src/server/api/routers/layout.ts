ca5e75625a8341f9cb4120cf82b190c30ec29dd7
Jon Greazel <jdgreazel@gmail.com>
2024-09-03 20:45:31 -0500

    fix build

--- src/server/api/routers/layout.ts
+++ src/server/api/routers/layout.ts
@@ -6,25 +6,27 @@ import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 
 import { filterUserForClient } from "../helpers/filterUsersForClient";
 
+// todo: come back once i have the permissions spec done
+// new plan: pry not use this. router match ui pages, utilize modules
+// this will pry go in a home or common router
 export const layoutRouter = createTRPCRouter({
-  getSideNav: privateProcedure
-    // no input, assumed to have user id in header since private proc
-    .query(async ({ ctx }) => {
-      const userId = ctx.userId;
-      // todo: come back once i have the permissions spec done
-      // const userRoleLinks = await ctx.db.user_Role.findMany({
-      //   where: {
-      //     userId: userId,
-      //   },
-      //   include: {
-      //     role: {
-      //       include: {
-      //         permissions: {
-      //           include: { permission: true },
-      //         },
-      //       },
-      //     },
-      //   },
-      // });
-    }),
+  // getSideNav: privateProcedure
+  //   // no input, assumed to have user id in header since private proc
+  //   .query(async ({ ctx }) => {
+  //     const userId = ctx.userId;
+  //     // const userRoleLinks = await ctx.db.user_Role.findMany({
+  //     //   where: {
+  //     //     userId: userId,
+  //     //   },
+  //     //   include: {
+  //     //     role: {
+  //     //       include: {
+  //     //         permissions: {
+  //     //           include: { permission: true },
+  //     //         },
+  //     //       },
+  //     //     },
+  //     //   },
+  //     // });
+  //   }),
 });
